5358611a30f9c6790f04b883aaf75d09a06ba6f6
preetsohal <preetsohal@live.com>
2016-07-14 12:56:45 -0700

    started but confused i might seek someone after class

---
+++ lab_gurpreet/test/events.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.readFile('first.txt',function(err,data){
+  if (err){
+    console.log(err);
+  }
+  else{
+    soncole.log('assync data is "+ data,toString'()};)
+  }
+})
