9db8f394a10904166b461583409866dea26b4366
preetsohal <preetsohal@live.com>
2016-07-15 12:51:08 -0700

    changed the name of the directory

--- lab_gurpreet/lib/index.js
+++
@@ -1,9 +0,0 @@
-var cowsay = require('cowsay');
-const newCowsay = module.exports = function(a){
-  return cowsay.say ({
-    text : a,
-    e : 'oO',
-    T : 'U'
-  });
-};
-console.log(newCowsay('moo'));
